36c2d699361bad5ee81c68c5cd75b4ba977e4f25
diegoapena <diego.apena@tecsup.edu.pe>
2026-06-04 12:09:45 -0500

    Arreglo de Escenario

--- Assets/EchoesOfMutations/ScriptsS10/Items/FlashLight.cs
+++ Assets/EchoesOfMutations/ScriptsS10/Items/FlashLight.cs
@@ -45,7 +45,7 @@ public class FlashLight : MonoBehaviour, IInteractable
 
     public void DetectPosition()
     {
-        if (Vector3.Distance(transform.position, GameManager.Instance.playerManager.transform.position) < 1.5f)
+        if (Vector3.Distance(transform.position, GameManager.Instance.playerManager.transform.position) < 2.5f)
         {
             GameManager.Instance.playerManager.playerMechanics.PickUp(gameObject);
             isInInventory = true; 
