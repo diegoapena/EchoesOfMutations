cc2196fc5be3053b17356cca45a2d3b244608179
wassup-999 <sebasquispeperez@gmail.com>
2026-06-13 22:44:39 -0500

    Organizing Enums

--- Assets/EchoesOfMutations/Scripts/Enums/EnemiesVarity.cs
+++
@@ -1,9 +0,0 @@
-public enum EnemiesVarity
-{
-    None,
-    Small,
-    Normal,
-    Strong,
-    Boss,
-    AllEnemies
-}
---
+++ Assets/EchoesOfMutations/Scripts/Enums/Enums.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public enum EnemiesVarity
+{
+    None,
+    Small,
+    Normal,
+    Strong,
+    Boss,
+    AllEnemies
+}
+public enum ItemsVarity
+{
+    None,
+    Interactable,
+    Craftable,
+    Material,
+    AllItems
+}
