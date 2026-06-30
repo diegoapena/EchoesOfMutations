d98c2b01e238e30b4be1e9269f32797840028545
wassup-999 <sebasquispeperez@gmail.com>
2026-06-30 05:52:22 -0500

    Sprites (ScriptableObjects)

--- Assets/EchoesOfMutations/Scripts/DataBase/Material/BaseMaterialData.cs
+++ Assets/EchoesOfMutations/Scripts/DataBase/Material/BaseMaterialData.cs
@@ -1,9 +1,12 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "BaseMaterialData", menuName = "EchoesOfMutations/BaseMaterialData")]
 public class BaseMaterialData : ScriptableObject
 {
+    [FoldoutGroup("Settings")]
     [SerializeField]private string materialName;
+    [FoldoutGroup("Settings"), PreviewField(150)]
     public Sprite icon;
 
     public string MaterialName => materialName;
--- Assets/EchoesOfMutations/Scripts/Enemy/SmallEnemy.cs
+++ Assets/EchoesOfMutations/Scripts/Enemy/SmallEnemy.cs
@@ -1,16 +1,83 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class SmallEnemy : MonoBehaviour
+public class SmallEnemy : BaseEnemy
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Action OnDead;
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void OnEnable()
+    {
+        BaseEnemy.OnDeath += OnDead;
+    }
+    private void OnDisable()
+    {
+        BaseEnemy.OnDeath -= OnDead;
+    }
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        DeadEnemy();
+        Attack();
+        NextTarget();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Barricade"))
+        {
+            if (CurrentBarricade == null)
+            {
+                CurrentBarricade = other.gameObject.GetComponent<Barricade>();
+                barricades.Add(other.gameObject.GetComponent<Barricade>());
+            }
+
+            FindBarricade();
+
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Barricade"))
+        {
+            if (CurrentBarricade != null)
+            {
+                agent.SetDestination(CurrentBarricade.transform.position);
+                agent.stoppingDistance = 2;
+            }
+        }
+    }
+    private void DeadEnemy()
+    {
+        if (health <= 0)
+        {
+            OnDead?.Invoke();
+            Debug.Log("dead");
+            if (GetComponent<NavMeshAgent>().enabled == true)
+            {
+                GetComponent<NavMeshAgent>().isStopped = true;
+                GetComponent<NavMeshAgent>().ResetPath();
+                GetComponent<NavMeshAgent>().enabled = false;
+            }
+            Destroy(gameObject, 1f);
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, 2f);
+
+        Gizmos.color = Color.blueViolet;
+        Gizmos.DrawWireSphere(transform.position, 2.5f);
     }
 }
